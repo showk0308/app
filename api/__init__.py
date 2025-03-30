be6d95b07c15b7ac5fa425174e40e998279b29d7
show <show0308@outlook.jp>
2025-03-30 21:51:56 +0900

    ６回目修正

--- api/__init__.py
+++
@@ -1,5 +0,0 @@
-# from fastapi import APIRouter
-# from endpoints.actuator_endpoints import some_endpoint
-
-# router = APIRouter()
-# router.include_router(some_endpoint.router, prefix="/somepath", tags=["some"])
